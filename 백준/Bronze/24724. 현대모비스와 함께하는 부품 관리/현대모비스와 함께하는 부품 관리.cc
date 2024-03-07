f36ec23087a7410ce08f05f22ed65526365770e9
Dong hun <88104352+Fletacode@users.noreply.github.com>
2024-03-07 22:35:46 +0900

    [Bronze III] Title: 현대모비스와 함께하는 부품 관리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cc
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int T;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+
+	cin >> T;
+
+	int cnt = 1;
+
+	while (T--) {
+		cout << "Material Management " << cnt++ <<"\n";
+		cout << "Classification ---- End!\n";
+	}
+
+}
