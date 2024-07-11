ef0c84ce09ccf17164f85d12c464bf417ab11825
Dong hun <88104352+Fletacode@users.noreply.github.com>
2024-07-11 15:46:14 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 4 ms, Memory: 2284 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.cc
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.cc
@@ -1,42 +1,42 @@
 #include<bits/stdc++.h>
 
+
 using namespace std;
 
+int N;
 
 int MAP[130][130];
 int V[130][130];
 
-int my[4] = {-1,1,0,0}; //상하좌우 
+int my[4] = {-1,1,0,0}; //상하좌우
 int mx[4] = {0,0,-1,1};
-queue<pair<int,pair<int,int>>> q;
-int N;
 
-void bfs(){
+void diestra(){
+    priority_queue<pair<int,pair<int,int>> > pq;
 
-    while(!q.empty()) q.pop();
-    
-
-    q.push({MAP[0][0],{0,0}});
+    pq.push({-MAP[0][0],{0,0}});
     V[0][0] = MAP[0][0];
 
-    
-    while(!q.empty()){
-        pair<int,int> now = q.front().second;
-        int val = q.front().first;
-        q.pop();
 
-        
+    while(!pq.empty()){
+        int y = pq.top().second.first;
+        int x = pq.top().second.second;
+        int val = -pq.top().first;
+        pq.pop();
+
+
         for (int i = 0; i < 4; i++){
-            int dy = my[i] + now.first;
-            int dx = mx[i] + now.second;
+            int dy = y + my[i];
+            int dx = x + mx[i];
             if (dy < 0 || dy >= N || dx < 0 || dx >= N) continue;
-            if (MAP[dy][dx] + val >= V[dy][dx]) continue;
-
-            q.push({MAP[dy][dx] + val,{dy,dx}});
-            V[dy][dx] = MAP[dy][dx] + val;
+            int next_val = val + MAP[dy][dx];
+            if (V[dy][dx] > next_val){
+                V[dy][dx] = next_val;
+                pq.push({-next_val,{dy,dx}});
+                
+            }
 
         }
-
 
     }
 
@@ -45,19 +45,20 @@ void bfs(){
 
 
 int main(){
-
-    ios_base::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false); 
     cin.tie(0); cout.tie(0);
     int cnt = 1;
+
     while(true){
-
-
         cin >> N;
-        if (N ==0) break;
+        if (N == 0) break;
 
         for (int i = 0; i < 126; i++) memset(MAP[i],0,sizeof(MAP[i]));
-        for (int i = 0; i < 126; i++) memset(V[i],98765432,sizeof(V[i]));
-        
+        for (int i = 0; i < 126; i++) {
+            for (int j = 0; j < 126; j++){
+                V[i][j] = 1e9;
+            }
+        }
 
         for (int i = 0; i < N; i++){
             for (int j = 0; j < N; j++){
@@ -65,14 +66,11 @@ int main(){
             }
         }
 
-        bfs();
+        diestra();
 
-        cout << "Problem "<< cnt++ << ": " << V[N-1][N-1] << "\n";
+        cout <<"Problem " << cnt++ << ": " << V[N-1][N-1] << "\n";
 
     }
 
-    
 
-
-    return 0;
 }
