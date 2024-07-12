545daec9692fb1961eee466ffa77cd84409913be
Dong hun <88104352+Fletacode@users.noreply.github.com>
2024-07-12 16:58:00 +0900

    [Gold I] Title: 최솟값과 최댓값, Time: 132 ms, Memory: 9052 KB -BaekjoonHub

--- 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.cc
+++ 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.cc
@@ -1,98 +1,82 @@
 #include<bits/stdc++.h>
 
-#define SIZE 262150
+typedef long long ll;
 
 using namespace std;
 
-typedef long long ll;
-
 int N,M;
-int a, b;
 
-ll arr[100010];
-ll min_tree[SIZE];
-ll max_tree[SIZE];
+ll A[100010];
+ll T_max[100010 * 4];
+ll T_min[100010 * 4];
 
+ll max_init(int node,int st,int end){
+    if (st == end) return T_max[node] = A[st];
 
-ll init_min(int node, int st, int end) {
+    ll mid = (st+end) / 2;
 
-	if (st == end) {
-		return min_tree[node] = min(min_tree[node],arr[st]);
-	}
-
-	int mid = (st + end) / 2;
-
-	return min_tree[node] = min(init_min(node * 2, st, mid),
-		init_min(node*2 + 1,mid+1,end));
+    return T_max[node] = max(max_init(node*2,st,mid) , max_init(node*2+1, mid+1, end));
 
 }
 
-ll init_max(int node, int st, int end) {
+ll min_init(int node,int st,int end){
+    if (st == end) return T_min[node] = A[st];
 
-	if (st == end) {
-		return max_tree[node] = max(max_tree[node], arr[st]);
-	}
+    ll mid = (st+end) / 2;
 
-	int mid = (st + end) / 2;
+    return T_min[node] = min(min_init(node*2,st,mid) , min_init(node*2+1, mid+1, end));
 
-	return max_tree[node] = max(init_max(node * 2, st, mid),
-		init_max(node * 2 + 1, mid + 1, end));
+}
+
+ll min_find(int node,int st, int end, int left, int right){
+
+    if (end < left || right < st) return 1e9;
+    if (left <= st && end <= right) return T_min[node];
+
+    ll mid = (st+end) / 2;
+
+    ll leftTree = min_find(node*2,st,mid,left,right);
+    ll rightTree = min_find(node*2 + 1, mid+1, end, left,right);
+
+    return min(leftTree , rightTree);
+
+}
+
+ll max_find(int node,int st, int end, int left, int right){
+
+    if (end < left || right < st) return -1;
+    if (left <= st && end <= right) return T_max[node];
+
+    ll mid = (st+end) / 2;
+
+    ll leftTree = max_find(node*2,st,mid,left,right);
+    ll rightTree = max_find(node*2 + 1, mid+1, end, left,right);
+
+    return max(leftTree , rightTree);
 
 }
 
 
-ll find_min(int node, int st, int end, int left, int right) {
 
-	if (st > right || end < left) return 1000000009;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
 
-	if (st >= left && end <= right) return min_tree[node];
+    cin >> N >> M;
 
-	int mid = (st + end) / 2;
+    for (int i = 0; i < N; i++){
+        cin >> A[i];
+    }
 
-	
-	
+    max_init(1,0,N-1);
+    min_init(1,0,N-1);
+    //for (int i = 1; i <= 10; i++) cout << T_min[i] << " ";
 
-	return min(find_min(node * 2, st, mid, left, right),
-		find_min(node * 2 + 1, mid + 1, end, left, right));
-
-
-}
-
-ll find_max(int node, int st, int end, int left, int right) {
-
-	if (st > right || end < left) return -1;
-
-	if (st >= left && end <= right) return max_tree[node];
-
-	int mid = (st + end) / 2;
-
-	return max(find_max(node * 2, st, mid, left, right),
-		find_max(node * 2 + 1, mid + 1, end, left, right));
-}
-
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
-
-	
-
-	
-	fill(min_tree, min_tree + SIZE, 1000000009);
-	fill(max_tree, max_tree + SIZE, -1);
-
-	cin >> N >> M;
-
-
-	for (int i = 0; i < N; i++) {
-		cin >> arr[i];
-	}
-	
-	init_min(1, 0, N-1);
-	init_max(1, 0, N - 1);
-
-	while (M--) {
-		cin >> a >> b;
-		cout << find_min(1, 0, N - 1, a - 1, b - 1) << " " << find_max(1, 0, N - 1, a - 1, b - 1) << "\n";
-	}
-	
+    
+    int a,b;
+    for (int i = 0; i < M; i++){
+        cin >> a >> b;
+        cout << min_find(1,0,N-1,a-1,b-1) << " " << max_find(1,0,N-1,a-1,b-1) << "\n";
+    }
+    
 }
