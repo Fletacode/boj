49cea536db7a56c59bbb2644eabcf49efd132be6
Dong hun <88104352+Fletacode@users.noreply.github.com>
2024-07-18 15:20:19 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+
+
+using namespace std;
+
+int N;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    cin >> N;
+
+    for (int i = 0; i < N;i++){
+        cout << "SciComLove" << "\n";
+    }
+
+
+}
