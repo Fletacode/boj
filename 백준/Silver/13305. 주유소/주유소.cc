fa78a892651101154b6a8506d632046531828fec
Dong hun <88104352+Fletacode@users.noreply.github.com>
2024-07-25 14:11:46 +0900

    [Silver III] Title: 주유소, Time: 24 ms, Memory: 4364 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -16,20 +16,27 @@ int main(){
     cin.tie(0); cout.tie(0);
 
     cin >> N;
-
+    ll total_dis = 0;
     for (int i = 1; i < N; i++){
         cin  >> dis[i];
+        total_dis += dis[i];
     }
+
     for (int i = 0; i < N; i++){
         cin >> val[i];
     }
 
     DP[1] = dis[1] * val[0];
 
+    ll min_val = min(val[0],val[1]);
+
     for (int i = 2; i < N; i++){
-        DP[i] = min(DP[i-1] + (val[i-1] * dis[i]),
-                    DP[i-1] + (val[i-2] * dis[i]) );
+        
+        DP[i] = DP[i-1] + (min_val * dis[i]);
+        min_val = min(val[i],min_val);
+        //cout << DP[i] << " ";
     }
+    //cout << "\n";
 
     cout << DP[N-1];
 
