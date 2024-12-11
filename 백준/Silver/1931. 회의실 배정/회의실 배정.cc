f3ed3dc2b72ac82a859103136830e8e6506a444a
Dong hun <88104352+Fletacode@users.noreply.github.com>
2024-12-11 19:35:21 +0900

    [Silver I] Title: 회의실 배정, Time: 24 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -7,59 +7,41 @@ int N;
 
 vector<pair<int,int>> v;
 
-bool compare(pair<int,int> a,pair<int,int> b){
-    if (a.first < b.first) return 1;
-    else{
-        if (a.first == b.first){
-            if (a.second < b.second){
-                return 1;
-            }else{
-                return 0;
-            }
-        }else{
-            return 0;
-        }
-    }
-}
-
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
-
+    
     cin >> N;
-
-    int a,b;
-    for (int i = 0 ; i < N; i++){
-        cin >> a >> b;
-        v.push_back({a,b});
+    
+    for (int i = 0; i < N; i++){
+        int st, end;
+        cin >> st >> end;
+        v.push_back({st,end});
     }
-
-    sort(v.begin(), v.end(),compare);
-
-    int cnt = 0;
+    
+    sort(v.begin(), v.end());
+    
+    
     int st = v[0].first;
     int end = v[0].second;
-    cnt++;
-
-
+    
+    int ans = 1;
+    
     for (int i = 1; i < v.size(); i++){
-        //교체
-        if (v[i].second < end){
-            if (v[i].second - v[i].first < end-st
-             && end >= v[i].second){
-                st = v[i].first;
-                end = v[i].second; 
-                continue;
-             }
-            
-        }
-
-        if (end <= v[i].first){
-            cnt++;
-            st = v[i].first;
-            end = v[i].second;
+        int vst = v[i].first;
+        int vend = v[i].second;
+        
+        //추가
+        if (end <= vst){
+            ans += 1;
+            st = vst;
+            end = vend;
+        } else if (end > vend){
+            st = vst;
+            end = vend;
         }
     }
-
-    cout << cnt;
+    
+    cout << ans;
+    
 }
