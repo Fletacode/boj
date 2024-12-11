cce43d066b393ea7ce1acb41b02052950699d32f
Dong hun <88104352+Fletacode@users.noreply.github.com>
2024-12-11 19:45:35 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 6008 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -1,41 +1,29 @@
 #include<bits/stdc++.h>
 
-
 using namespace std;
 
-int dp[1001][1001];
+string str1;
+string str2;
 
-string s1,s2;
+int dp[1010][1010];
 
-
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
-
-	cin >> s1 >> s2;
-
-	//cout << s1 << " " << s2;
-
-	
-	for (int i = 1; i <= s1.size(); i++) {
-		for (int j = 1; j <= s2.size(); j++) {
-			if (s1[i-1] == s2[j-1]) {
-				dp[i][j] = max(dp[i][j], dp[i - 1][j-1] + 1);
-			}
-			else {
-				dp[i][j] = max(dp[i][j], max(dp[i][j-1],dp[i-1][j]));
-			}
-		}
-	}
-	/*
-	for (int i = 0; i <= s1.size(); i++) {
-		for (int j = 0; j <= s2.size(); j++) {
-			cout << dp[i][j] << " ";
-		}
-		cout << "\n";
-	}
-	*/
-
-	cout << dp[s1.size()][s2.size()];
-	
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    
+    cin >> str1 >> str2;
+    
+    for (int i = 1; i <= str1.size(); i++){
+        for (int j = 1; j <= str2.size(); j++){
+            if (str1[i-1] == str2[j-1]){
+                dp[i][j] = max(dp[i][j], dp[i-1][j-1] + 1); 
+            } else{
+                dp[i][j] = max(dp[i][j], max(dp[i-1][j], dp[i][j-1]) );
+            }
+            
+        }
+    }
+    
+    cout << dp[str1.size()][str2.size()];
+    
 }
