8719ff4584277876d164259f3f04b7a3f8cb4bf5
Dong hun <88104352+Fletacode@users.noreply.github.com>
2024-12-11 15:55:00 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -1,27 +1,22 @@
 #include<bits/stdc++.h>
 
-
 using namespace std;
 
 int N;
 
-int MOD = 10007;
+int DIV = 10007;
 
 int dp[1010];
 
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-
     cin >> N;
 
     dp[1] = 1;
     dp[2] = 2;
 
     for (int i = 3; i <= N; i++){
-        dp[i] = (dp[i-1] % MOD) + (dp[i-2] % MOD);
+        dp[i] = (dp[i-1] + dp[i-2]) % DIV;
     }
 
-
-    cout << dp[N] % MOD;
+    cout << dp[N] % DIV;
 }
