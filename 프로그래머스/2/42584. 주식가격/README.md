894392c63d288a9f87e5942a48a9e78d457c969f
Dong hun <88104352+Fletacode@users.noreply.github.com>
2025-02-07 16:18:06 +0900

    [level 2] Title: 주식가격, Time: 27.56 ms, Memory: 26.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584#qna) 
 
 ### 성능 요약
 
-메모리: 26.8 MB, 시간: 26.81 ms
+메모리: 26.8 MB, 시간: 27.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 07일 15:24:26
+2025년 02월 07일 16:18:03
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -1,27 +1,41 @@
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+stack<pair<int,int>> st;
+
 vector<int> solution(vector<int> prices) {
-    vector<int> ans;
+    vector<int> ans(prices.size(),0);
     
-    for (int i = 0; i < prices.size() - 1; i++) {
-        
-        int j = i + 1;
-        
-        int now = prices[i];
-        
-        while (now <= prices[j]) {
-            if (j >= prices.size() - 1) break;
-            j++;
+    for (int i = 0; i < prices.size() - 1 ; i++) {
+        if (st.empty()) st.push({i,prices[i]});
+        else {
+            if (st.top().second > prices[i]) {
+                while(!st.empty() && st.top().second > prices[i]) {
+                    int idx = st.top().first;
+                    //cout << idx << " " << i;
+                    ans[idx] = i - idx;
+                    st.pop();
+                }
+                st.push({i,prices[i]});
+            } else {
+                //cout << i << " " << prices[i] << "\n";
+                st.push({i,prices[i]});
+            }
+            
+            
         }
-        
-        ans.push_back(j-i);
-        
     }
     
-    ans.push_back(0);
+ 
+    
+    while(!st.empty()) {
+        pair<int,int> p = st.top();
+        //cout << p.first << "\n";
+        ans[p.first] = prices.size() - 1 - p.first;
+        st.pop();
+    }
+    
     
     return ans;
 }
