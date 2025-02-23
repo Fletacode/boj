0e023d7dbf4fec1a4af5f224ba8a5482e484f6ca
Dong hun <88104352+Fletacode@users.noreply.github.com>
2025-02-23 17:56:56 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -1,21 +1,21 @@
-WITH RECURSIVE family AS (
+with recursive ret as (
+    select A.id, A.parent_id, 1 as gen
+    from ECOLI_DATA as A
+    where IFNULL(A.parent_id,0) = 0
     
-    select id, parent_id, 1 as gen
-    from ECOLI_DATA
-    where parent_id is null
- 
     union all
- 
-    select  t1.id, t1.parent_id, t2.gen + 1
-    from ECOLI_DATA as t1
-    inner join family as t2
-    on t2.id = t1.parent_id
- )
- 
- 
-select count(A.gen) as "COUNT", A.gen as "GENERATION" from family as A
-left join ECOLI_DATA as B
-on A.id = B.parent_id
-where B.id is null
-group by A.gen
-order by A.gen;
+    
+    select T.id, T.parent_id, ret.gen + 1
+    from ECOLI_DATA as T
+    inner join ret
+    on T.parent_id = ret.id
+)
+
+
+
+select count(*) as 'COUNT', ret.gen as 'GENERATION' from ret
+left join ECOLI_DATA as A
+on ret.id = A.parent_id
+where A.parent_id is null
+group by A.parent_id, ret.gen
+order by ret.gen ASC
