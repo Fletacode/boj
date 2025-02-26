90b4adb681a317c752f32132bb08388e37211e9c
Dong hun <88104352+Fletacode@users.noreply.github.com>
2025-02-26 21:05:50 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -1,42 +1,18 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-typedef long long ll;
 
-int ans = 1;
-
-map<string,int> Map;
-
-vector<int> v;
-
-void make_map(vector<vector<string>> &clothes){
-    
-    for (int i = 0; i < clothes.size(); i++) {
-        
-        Map[clothes[i][1]] += 1;
-        
-    }
-    
-    for (auto iter = Map.begin(); iter != Map.end(); iter++) {
-        v.push_back((*iter).second);
-        //cout << (*iter).second;
-    }
-    
-}
-
-void print_v(){
-    for (int i = 0; i < v.size(); i++){
-        cout << v[i] << " ";
-    }
-}
-
+map<string,int> mp;
 
 int solution(vector<vector<string>> clothes) {
+    int ans = 1;
     
-    make_map(clothes);
+    for (int i = 0; i < clothes.size(); i++) {
+        mp[clothes[i][1]] = mp[clothes[i][1]] + 1;
+    }
     
-    for (int i = 0; i < v.size(); i++){
-        ans *= (v[i] + 1);
+    for (auto iter : mp) {
+        ans *= iter.second + 1;
     }
     
     ans -= 1;
